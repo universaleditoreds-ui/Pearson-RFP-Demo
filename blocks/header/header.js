f95b8ee5c9b87a1d2c5a7cc4cdb9e201c81666bc
Guruprasad H D <guruprasad.h.d@ibm.com>
2026-01-30 04:09:53 +0530

    added header css icon

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -68,10 +68,16 @@ export default async function decorate(block) {
     '<div>',
     '<button class="locale-selection__button selected_en-US" aria-haspopup="true" aria-expanded="false">',
     selectedCountry,
+    '<svg class="ls-chevron" width="16" height="16" viewBox="0 0 24 24" ',
+    'fill="none" stroke="currentColor" stroke-width="2" ',
+    'stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">',
+    '<polyline points="6 9 12 15 18 9"></polyline>',
+    '</svg>',
     '</button>',
     '</div>',
     '</div>',
     '</div>',
+
 
     '</div>',
     '</div>',
