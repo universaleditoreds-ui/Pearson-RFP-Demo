e5d38f01653ead7372c350668df66dd53a7d26a9
universaleditoreds-ui <universaleditoreds@gmail.com>
2026-02-23 15:16:34 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -224,23 +224,6 @@ export default async function decorate(block) {
     headercontainer.insertAdjacentHTML('beforeend', headerHTML);
   }
 
-  function initStickyHeader() {
-    const headerEL = document.querySelector('header');
-    const stickyEl = document.querySelector('.he-pr-nav__wrapper');
-
-    if (!headerEL || !stickyEl) return;
-
-    const stickyOffset = stickyEl.offsetTop;
-
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= stickyOffset) {
-        headerEL.classList.add('isSticky');
-      } else {
-        headerEL.classList.remove('isSticky');
-      }
-    });
-  }
-
-  initStickyHeader();
+ 
 
 }
